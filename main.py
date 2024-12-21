33b8bf69de6688b66f0d9e611528773d02d9fce0
Louzani mohamed adam <mm_louzani@esi.dz>
2024-12-21 03:04:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,4 +11,7 @@ app = FastAPI()
 @app.get("/")
 def reaf_root():
   return 'pdd'
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
 
